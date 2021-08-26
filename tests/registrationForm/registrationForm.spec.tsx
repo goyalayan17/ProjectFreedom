9c8d021da615431e39c783c7582181897001ec7f
Ayan Goyal <goyal.ayan17@gmail.com>
2021-08-26 17:47:28 +0530

    added props in registration form spec file

--- tests/registrationForm/registrationForm.spec.tsx
+++ tests/registrationForm/registrationForm.spec.tsx
@@ -1,19 +1,23 @@
-import Enzyme,{ mount } from "enzyme";
+import Enzyme, { mount } from "enzyme";
 import React from "react";
 import { IRegistrationProps, Registration } from "../../src/domian/registrationForm/components/Registration";
 import { IRegistrationForm } from "../../src/domian/registrationForm/interfaces/IRegistrationForm";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
 const mockOnChange: jest.Mock<any> = jest.fn();
 const mockOnSubmit: jest.Mock<any> = jest.fn();
 const mockOnEdit: jest.Mock<any> = jest.fn();
+const mockOnAgreeUpdate: jest.Mock<any> = jest.fn();
+const mockOnDelete: jest.Mock<any> = jest.fn();
 
 const props: IRegistrationProps = {
     onChange: mockOnChange,
     onSubmit: mockOnSubmit,
     onEdit: mockOnEdit,
+    onAgreeUpdate: mockOnAgreeUpdate,
+    onDelete: mockOnDelete,
     registrationList: [],
     registrationForm: {} as IRegistrationForm
 };
