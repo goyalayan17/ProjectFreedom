3de385d887041b01307337257372dc9c784da563
Ayan Goyal <goyal.ayan17@gmail.com>
2022-01-27 16:02:15 +0530

    updated storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-    stories: ['../stories/**/*.story.tsx'],
-    // addons: ['@storybook/addon-knobs'],
-  };
+  stories: ['../stories/**/*.story.tsx'] // addons: ['@storybook/addon-knobs'],
+  ,
+  core: {
+    builder: "webpack5"
+  }
+};
